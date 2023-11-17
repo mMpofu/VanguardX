8fd3c8123bd0887b6115c104f80becf3b3db7948
Musa <mpoufumusa10@gmail.com>
2023-11-17 12:00:35 +0200

    set to no inspection for deprecations

--- app/src/main/java/com/example/vanguardx/model/db/AppDatabase.java
+++ app/src/main/java/com/example/vanguardx/model/db/AppDatabase.java
@@ -24,6 +24,7 @@ import java.util.Objects;
 import java.util.SortedMap;
 
 
+/** @noinspection ALL*/
 @Database(entities = {AppUsageEntity.class, InstalledAppEntity.class},version = 1,exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     private static final String TAG = "AppDatabase";
