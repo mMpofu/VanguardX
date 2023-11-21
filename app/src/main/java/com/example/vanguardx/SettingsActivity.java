5b4547affbc399fec32ef5a1f90bb403c4b0e34e
Musa <mpoufumusa10@gmail.com>
2023-11-21 17:57:14 +0200

    admin app privileges to avoid unistall

--- app/src/main/java/com/example/vanguardx/SettingsActivity.java
+++ app/src/main/java/com/example/vanguardx/SettingsActivity.java
@@ -1,5 +1,9 @@
 package com.example.vanguardx;
 
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
@@ -8,10 +12,20 @@ import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE = 0;
+    /** @noinspection deprecation*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+        DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+        ComponentName deviceAdmin = new ComponentName(this, YourAdminReceiver.class);
+        if (!dpm.isAdminActive(deviceAdmin)) {
+            Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, deviceAdmin);
+            startActivityForResult(intent, REQUEST_CODE);
+        }
+
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.settings, new SettingsFragment())
---
+++ app/src/main/java/com/example/vanguardx/YourAdminReceiver.java
@@ -0,0 +1,23 @@
+package com.example.vanguardx;
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class YourAdminReceiver extends DeviceAdminReceiver {
+
+    @Override
+    public void onEnabled(Context context, Intent intent) {
+        Toast.makeText(context, "Device Admin Enabled", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public CharSequence onDisableRequested(Context context, Intent intent) {
+        return "Are you sure you want to disable device admin?";
+    }
+
+    @Override
+    public void onDisabled(Context context, Intent intent) {
+        Toast.makeText(context, "Device Admin Disabled", Toast.LENGTH_SHORT).show();
+    }
+}
