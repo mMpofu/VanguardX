80f613269ffc8a344f07ba11673efc96c43fc47d
Musa <mpoufumusa10@gmail.com>
2023-11-22 14:08:04 +0200

    added try exception

--- app/src/main/java/com/example/vanguardx/utilities/AppUtils.java
+++ app/src/main/java/com/example/vanguardx/utilities/AppUtils.java
@@ -6,15 +6,27 @@ import android.content.pm.PackageManager;
 
 public class AppUtils {
     public static void disableApp(Context context, String packageName, String activityName) {
-        PackageManager packageManager = context.getPackageManager();
-        ComponentName componentName = new ComponentName(packageName, activityName);
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            ComponentName componentName = new ComponentName(packageName, activityName);
 
-        packageManager.setComponentEnabledSetting(
-                componentName,
-                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-                PackageManager.DONT_KILL_APP
-        );
+            // Check if the component is enabled before disabling
+            if (packageManager.getComponentEnabledSetting(componentName)
+                    != PackageManager.COMPONENT_ENABLED_STATE_DISABLED) {
+
+                // Disable the component
+                packageManager.setComponentEnabledSetting(
+                        componentName,
+                        PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                        PackageManager.DONT_KILL_APP
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Handle the exception (e.g., log it)
+        }
     }
+
 
     public static void enableApp(Context context, String packageName, String activityName) {
         PackageManager packageManager = context.getPackageManager();
